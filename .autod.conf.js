bdfe776001450c284e3a8188fa7cd01ed54cb9ed
lemonlwz <lemonlwz@gamil.com>
2017-02-21 21:09:06 +0800

    init

---
+++ .autod.conf.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  write: true,
+  prefix: '^',
+  test: [
+    'test',
+    'benchmark',
+  ],
+  dep: [
+    'egg'
+  ],
+  devdep: [
+    'egg-ci',
+    'egg-bin',
+    'autod',
+    'eslint',
+    'eslint-config-egg',
+    'supertest',
+    'webstorm-disable-index',
+  ],
+  exclude: [
+    './test/fixtures',
+    './dist',
+  ],
+  registry: 'https://r.cnpmjs.org',
+};
+
